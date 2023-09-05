aee87458ed0a53be694bb02eb5d3ce855f8e171f
Ahmed Abdelhak <112263541+Ahmed200644@users.noreply.github.com>
2023-09-05 15:54:17 +0300

    Add files via upload

---
+++ Aasr Progriming - Copy/frist project/main.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <array>
+#include <limits.h>
+using namespace std;
+
+int main()
+{
+      /*int num;
+        cin >> num;
+
+        if (num % 2 == 0)
+        cout << "The Number" << num << "Is Even\n";
+        else
+        cout << "The Number" << num << "Is odd\n";*/
+     /* int a, b, c;
+        cin >> a >> b >>c;
+
+        if (a > b && a > c)
+        {
+            cout << a << "Is The Greatest number\n";
+        }
+        else if (b > a && b > c)
+        {
+        cout << b << "Is The Greatest number\n";
+        }
+        else
+        {
+        cout << c << "Is The Greatest number\n";
+        }*/
+/*
+        int points;
+        cin >> points;
+
+        if (points > 0 && points <= 500)
+        {
+            cout << "not bad\n";
+        }
+        else if (points > 500 && points <= 1000)
+        {
+            cout << "Very Good\n";
+        }
+        else
+        {
+            cout << "VIP\n";
+        }
+*/
+
+  /*     int num_one, num_two, op;
+
+        cout << "Tpye Number One\n";
+        cin >> num_one;
+        cout << "Type Number two\n";
+        cin >> num_two;
+        cout << "Choose Operation Number\n";
+        cout << "[1] +\n";
+        cout << "[2] -\n";
+        cout << "[3] /\n";
+        cout << "[4] *\n";
+        cin >>op;
+
+        if (op == 1)
+        {
+           cout << num_one + num_two << "\n";
+        }
+        else if (op == 2)
+        {
+            cout << num_one - num_two << "\n";
+        }
+        else if (op == 3)
+        {
+            cout << num_one / num_two << "\n";
+        }
+
+        else if (op == 4)
+        {
+            cout << num_one * num_two << "\n";
+        }
+
+        else
+        {
+            cout << "Operation Is Not Valid\n";
+        }
+
+
+*|
+
+        return 0;
+}
